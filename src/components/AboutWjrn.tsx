d2fbd2c9e28a136737e08f87e7e2c0720b81479a
Jace Brown <jacesemail@gmail.com>
2026-07-24 02:04:04 -0700

    deploy: 2026-07-24 02:04

--- src/components/AboutWjrn.tsx
+++ src/components/AboutWjrn.tsx
@@ -147,10 +147,10 @@ export default function AboutWjrn({ STATIONS }: AboutWjrnProps) {
       </section>
 
       {/* Team */}
-      <section className="relative z-10 w-full max-w-7xl mx-auto -mt-[13.3px]">
+      <section className="relative z-10 w-full max-w-7xl mx-auto -mt-[27.475px]">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {TEAM.map((member, idx) => (
-            <div key={idx} className="flex flex-col gap-[16.2px]">
+            <div key={idx} className="flex flex-col gap-[12.15px]">
               <img
                 src={member.photo}
                 alt={member.name}
